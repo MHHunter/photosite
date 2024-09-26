b66bfe0c18622fb1f2c3ffdd59ffb7dcd9c5d256
mohammad <mh.m.dev81@gmail.com>
2024-09-26 14:08:05 +0330

    front done

--- PhotoSite/PhotoSite/urls.py
+++ PhotoSite/PhotoSite/urls.py
@@ -18,13 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('accounts/', include('allauth.urls')),
-    path('', include('photos.urls')),  # Include the URLs from the 'photos' app
+    path('accounts/', include('allauth.urls')),
+    path('', include('photos.urls')),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
